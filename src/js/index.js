bb662d25b0099abca8e5f13a2d449cca10aa3c0a
GBressler <gregbressler@gmail.com>
2019-04-09 14:13:50 -0500

    Build recipe views

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
-import * as searchView from './views/searchView'
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /* Global state
@@ -72,7 +73,8 @@ const controlRecipe = async () => {
   
   if (id) {
     //Prep UI for Changes
-    
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
     //Create new recipe obj
     state.recipe = new Recipe(id);
     try {
@@ -84,7 +86,8 @@ const controlRecipe = async () => {
     state.recipe.calcTime();
     state.recipe.calcServings();
     //render recipe
-    console.log(state.recipe);
+    clearLoader();
+    recipeView.renderRecipe(state.recipe);
     } catch (error) {
       alert('There was an processing error!');
     }
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -10,7 +10,7 @@ export default class Recipe {
     try {
       const res = await axios(`${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
       this.title = res.data.recipe.title;
-      this.author = res.data.recipe.author;
+      this.author = res.data.recipe.publisher;
       this.img = res.data.recipe.image_url;
       this.url = res.data.recipe.source_url;
       this.ingredients = res.data.recipe.ingredients;
@@ -34,6 +34,7 @@ export default class Recipe {
   parseIngredients(){
     const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
     const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+    const units = [...unitsShort, 'kg', 'g'];
 
     const newIngredients = this.ingredients.map(el =>{
       //1) Uniform Ingredients
@@ -46,7 +47,7 @@ export default class Recipe {
       ingredient = ingredient.replace(/ *\([^]*\) */g, '');
       //3) Parse ingredients into count, unit and ingredient
       const arrIng = ingredient.split(' ');
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
       
       let objIng;
       if (unitIndex > -1) {
