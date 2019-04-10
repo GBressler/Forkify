23fa8134e353102303e2e164dca29104dfaeac49
GBressler <gregbressler@gmail.com>
2019-04-10 16:09:38 -0500

    build shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -80,6 +81,7 @@ const controlRecipe = async () => {
     try {
       //Get recipe data & parse Ingredients
       await state.recipe.getRecipe();
+      console.log(state.recipe.ingredients);
       state.recipe.parseIngredients();
 
     //Calculate serv & time
@@ -113,3 +115,5 @@ elements.recipe.addEventListener('click', e => {
   }
   console.log(state.recipe);
 });
+
+window.l = new List();
